b0818f1ba6c9e2e9fd6f5bf15a716bfd1fe07191
Alamusi Alamusi <alamusi@questflex.com>
2020-07-19 11:50:09 -0700

    added webassembly test with different optimization strategy

--- cal-wa.js
+++ cal-wa.js
@@ -6,24 +6,27 @@
 const fs = require('fs')
 const loader = require('@assemblyscript/loader')
 const imports = { /* imports go here */ }
-const wasmModule = loader.instantiateSync(fs.readFileSync(`${__dirname}/build/optimized.wasm`), imports).exports
 
-let start = Date.now()
-let result = wasmModule.fibo(40)
-let finish = Date.now()
-console.log('fibo', result, 'in webassembly runs', finish - start)
+module.exports = function (build) {
+  const wasmModule = loader.instantiateSync(fs.readFileSync(`${__dirname}${build}`), imports).exports
 
-start = Date.now()
-result = wasmModule.slow64(10)
-finish = Date.now()
-console.log('slow', result, 'in webassembly (64-bit) runs', finish - start)
+  let start = Date.now()
+  let result = wasmModule.fibo(40)
+  let finish = Date.now()
+  console.log('fibo', result, 'in webassembly runs', finish - start)
 
-start = Date.now()
-result = wasmModule.slow32(10)
-finish = Date.now()
-console.log('slow', result, 'in webassembly (32-bit) runs', finish - start)
+  start = Date.now()
+  result = wasmModule.slow64(10)
+  finish = Date.now()
+  console.log('slow', result, 'in webassembly (64-bit) runs', finish - start)
 
-start = Date.now()
-result = wasmModule.slowJS(10)
-finish = Date.now()
-console.log('slow', result, 'in webassembly (JSMath) runs', finish - start)
+  start = Date.now()
+  result = wasmModule.slow32(10)
+  finish = Date.now()
+  console.log('slow', result, 'in webassembly (32-bit) runs', finish - start)
+
+  start = Date.now()
+  result = wasmModule.slowJS(10)
+  finish = Date.now()
+  console.log('slow', result, 'in webassembly (JSMath) runs', finish - start)
+}
