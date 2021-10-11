aa0a908cbc04e2c8ac98b972d21cc96141a6f13a
alamusi <alamusi@gmail.com>
2021-10-10 19:21:17 -0700

    removed 32bit webassembly test to be consistent with other benchmark

--- assembly/index.ts
+++ assembly/index.ts
@@ -8,7 +8,7 @@ export function fibo (n: i32): i32 {
     return fibo(n - 2) + fibo(n - 1);
 }
 
-export function slow64 (n: f64): f64 {
+export function slow (n: f64): f64 {
   let result:f64 = 0
   let invN:f64 = 1.0 / n;
   let i:f64 = 0
@@ -20,29 +20,12 @@ export function slow64 (n: f64): f64 {
   return result
 }
 
-export function slow32 (n: f32): f32 {
-  let result:f32 = 0
-  let invN:f32 = 1.0 / n;
-  let i:f32 = 0
-  while (i < n) {
-    let x = NativeMathf.pow(i * invN, 7);
-    result += NativeMathf.atan(x) * NativeMathf.tan(x)
-    i++
-  }
-  return result
-}
-
 let start = Date.now()
 let result1 = fibo(40)
 let finish = Date.now()
 Console.log('fibo ' + result1.toString() + ' in webassembly runs ' + (finish - start).toString())
 
 start = Date.now()
-let result2 = slow64(10000000)
+let result2 = slow(10000000)
 finish = Date.now()
-Console.log('slow ' + result2.toString() + ' in webassembly (64-bit) runs ' + (finish - start).toString())
-
-start = Date.now()
-let result3 = slow32(10000000)
-finish = Date.now()
-Console.log('slow ' + result3.toString() + ' in webassembly (32-bit) runs ' + (finish - start).toString())
+Console.log('slow ' + result2.toString() + ' in webassembly runs ' + (finish - start).toString())
--- cal-wa.js
+++ cal-wa.js
@@ -4,23 +4,16 @@ const fs = require('fs')
 const { WASI } = require("wasi")
 const wasi = new WASI()
 
-module.exports = function (build) {
-  const imports = { wasi_snapshot_preview1: wasi.wasiImport }
-  const wasmBuffer = fs.readFileSync(`${__dirname}${build}`)
-  WebAssembly.instantiate(wasmBuffer, imports).then(wasmModule => {
-    let start = Date.now()
-    let result = wasmModule.instance.exports.fibo(40)
-    let finish = Date.now()
-    console.log('fibo', result, 'in webassembly runs', finish - start)
-  
-    start = Date.now()
-    result = wasmModule.instance.exports.slow64(10000000)
-    finish = Date.now()
-    console.log('slow', result, 'in webassembly (64-bit) runs', finish - start)
-  
-    start = Date.now()
-    result = wasmModule.instance.exports.slow32(10000000)
-    finish = Date.now()
-    console.log('slow', result, 'in webassembly (32-bit) runs', finish - start)
-  })
-}
+const imports = { wasi_snapshot_preview1: wasi.wasiImport }
+const wasmBuffer = fs.readFileSync(`${__dirname}/build/${process.argv[2] || 'optimized'}.wasm`)
+WebAssembly.instantiate(wasmBuffer, imports).then(wasmModule => {
+  let start = Date.now()
+  let result = wasmModule.instance.exports.fibo(40)
+  let finish = Date.now()
+  console.log('fibo', result, 'in webassembly runs', finish - start)
+
+  start = Date.now()
+  result = wasmModule.instance.exports.slow(10000000)
+  finish = Date.now()
+  console.log('slow', result, 'in webassembly runs', finish - start)
+})
